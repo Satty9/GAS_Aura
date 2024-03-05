cf4c3c36795eac51f5e0eb737da75531659ac24d
Oleg(Satty) <sattyjparts@gmail.com>
2024-03-05 13:46:49 +0200

    feat (player character): now player character move like in diablo
    # fix idle anim bug
    # add game mode
    # add spring arm and camera for player char in BP

--- Source/Aura/Private/Character/AuraPlayerCharacter.cpp
+++ Source/Aura/Private/Character/AuraPlayerCharacter.cpp
@@ -3,3 +3,16 @@
 
 #include "Character/AuraPlayerCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraPlayerCharacter::AAuraPlayerCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator (0.f, 400.f, 0.f); // rotation by Z with 400 speed
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
